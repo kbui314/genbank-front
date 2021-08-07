5ecb18c79753a635cb283535e4f36527ce273e72
Kenny Bui <kbui3145@gmail.com>
2021-08-07 16:39:02 -0700

    adhere to eslint formatting

--- genbank/src/app/accountlist/accountlist.service.ts
+++ genbank/src/app/accountlist/accountlist.service.ts
@@ -2,40 +2,34 @@ import { Injectable } from '@angular/core';
 import { Account } from './../model/account';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ThrowStmt } from '@angular/compiler';
 import { Transaction } from '../model/transaction';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountlistService {
-  
-  accounts:Account[];
-  url:String = "http://localhost:8080/";
-  
-  constructor(
-    private http:HttpClient
-  ) { }
+  accounts: Account[];
+  url = 'http://localhost:8080/';
 
-  getAccounts(){
-    return this.http.get<Account[]>(this.url+"viewaccounts");
+  constructor(private http: HttpClient) {}
+
+  getAccounts(): Observable<Account[]> {
+    return this.http.get<Account[]>(this.url + 'viewaccounts');
   }
 
-  generateNewAccount(){
-    return this.http.get<Account>(this.url+"addaccount");
+  generateNewAccount(): Observable<Account> {
+    return this.http.get<Account>(this.url + 'addaccount');
   }
 
-  deposit(trans:Transaction):Observable<Transaction>{
-    return this.http.post<Transaction>(this.url+"deposit",trans);
+  deposit(trans: Transaction): Observable<Transaction> {
+    return this.http.post<Transaction>(this.url + 'deposit', trans);
   }
 
-  withdraw(trans:Transaction):Observable<Transaction>{
-    return this.http.post<Transaction>(this.url+"withdraw",trans);
+  withdraw(trans: Transaction): Observable<Transaction> {
+    return this.http.post<Transaction>(this.url + 'withdraw', trans);
   }
 
-  tranfer(trans:Transaction):Observable<Transaction>{
-    return this.http.post<Transaction>(this.url+"transfer/",trans);
+  tranfer(trans: Transaction): Observable<Transaction> {
+    return this.http.post<Transaction>(this.url + 'transfer/', trans);
   }
-
 }
-
