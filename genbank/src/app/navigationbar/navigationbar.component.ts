8071aac5546150738101ee2470c811a7d758a253
Kenny Bui <kbui3145@gmail.com>
2021-08-07 20:14:42 -0700

    refactored navigationbar to eslint fomatting

--- genbank/src/app/navigationbar/navigationbar.component.ts
+++ genbank/src/app/navigationbar/navigationbar.component.ts
@@ -3,29 +3,29 @@ import { LoginService } from '../login/login.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-navigationbar',
-  templateUrl: './navigationbar.component.html',
-  styleUrls: ['./navigationbar.component.css']
+  selector:  'app-navigationbar',
+  templateUrl:  './navigationbar.component.html',
+  styleUrls:  ['./navigationbar.component.css']
 })
 export class NavigationbarComponent implements OnInit {
 
-  isLogin:boolean = false;
+  isLogin = false;
   constructor(
-    private loginService:LoginService,
-    private router:Router
+    private loginService: LoginService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    if(this.router.url == "/view"){
+    if (this.router.url === '/view'){
       this.isLogin = true;
     }else{
       this.isLogin = false;
     }
   }
 
-  logout(){
-    this.loginService.logout().subscribe(resp =>{
-      if(resp == null){
+  logout(): void{
+    this.loginService.logout().subscribe(resp => {
+      if (resp === null){
         this.loginService.setLogin(false);
         this.router.navigateByUrl('');
       }
