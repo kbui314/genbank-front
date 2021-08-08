fa0b7fd49806043d76766158e148eae5e2c45119
Kenny Bui <kbui3145@gmail.com>
2021-08-07 20:12:39 -0700

    refactors models to enlint formatting

--- genbank/src/app/model/account.ts
+++ genbank/src/app/model/account.ts
@@ -1,12 +1,12 @@
 import { Transaction } from './transaction';
 
 export class Account{
-    id:number;
-    accountnumber:string;
-    balance:number;
-    transactions:Transaction[];
+    id: number;
+    accountnumber: string;
+    balance: number;
+    transactions: Transaction[];
 
-    constructor(id:number, accountnumber:string, balance:number){
+    constructor(id: number, accountnumber: string, balance: number){
         this.id = id;
         this.accountnumber = accountnumber;
         this.balance = balance;
